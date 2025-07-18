3f078e27b3a55b1a48c9198d1a46c44260a2ddc8
hashimrmzn <hashimramzan246@gmail.com>
2025-07-18 17:16:38 +0500

    Updated: made some changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
+
   {
 
 function handleHeaderBg() {
